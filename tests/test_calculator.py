93058a9b67fdfd0e09acfcc8e134214a4d20ad34
Lucas Monteiro <lucasmonteiroferreira1@gmail.com>
2025-06-02 11:55:10 -0300

    Add tests for divide function

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -8,3 +8,8 @@ def test_subtract():
     assert subtract(3, 3) == 0
     assert subtract(6, 4) == 2
     assert subtract(5, 10) == -5
+
+def test_divide():
+    assert divide(9, 3) == 3
+    assert divide(24, 6) == 6
+    assert divide(-10, 5) == -2
