c65d6c934714f7c8749cf812acb119723d632589
Lucas Monteiro <lucasmonteiroferreira1@gmail.com>
2025-06-02 11:49:33 -0300

    Add multiply function to calculator

--- src/calculator.py
+++ src/calculator.py
@@ -3,3 +3,6 @@ def sum(a, b):
 
 def subtract(a, b):
     return a - b
+
+def multiply(a, b):
+    return a * b
--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -8,3 +8,8 @@ def test_subtract():
     assert subtract(3, 3) == 0
     assert subtract(6, 4) == 2
     assert subtract(5, 10) == -5
+
+def test_multiply():
+    assert multiply(2, 3) == 6
+    assert multiply(6, 6) == 36
+    assert multiply(-4, 5) == -20
