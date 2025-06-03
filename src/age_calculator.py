8a15210819754b389ab282b099eb901f370691fa
Lucas Monteiro <lucasmonteiroferreira1@gmail.com>
2025-06-03 14:24:43 -0300

    Mock today's date with @freeze_time when testing 'calculate_age' function

--- src/age_calculator.py
+++ src/age_calculator.py
@@ -1,10 +1,11 @@
 import datetime
 
 class AgeCalculator:
-    def calculate_age(date_of_birth, comparison_date=datetime.date.today()):
-        age = comparison_date.year - date_of_birth.year
+    def calculate_age(date_of_birth):
+        today = datetime.date.today()
+        age = today.year - date_of_birth.year
         
-        if (comparison_date.month, comparison_date.day) < (date_of_birth.month, date_of_birth.day):
+        if (today.month, today.day) < (date_of_birth.month, date_of_birth.day):
             age -= 1
 
         return age
--- tests/test_age_calculator.py
+++ tests/test_age_calculator.py
@@ -1,7 +1,8 @@
 from src.age_calculator import AgeCalculator
 import datetime
+from freezegun import freeze_time
 
+@freeze_time("2025-05-31")
 def test_calculate_age():
-    fixed_date = datetime.date(2025, 5, 31)
-    assert AgeCalculator.calculate_age(datetime.date(1994, 10, 19), fixed_date) == 30
-    assert AgeCalculator.calculate_age(datetime.date(2004, 8, 25), fixed_date) == 20
+    assert AgeCalculator.calculate_age(datetime.date(1994, 10, 19)) == 30
+    assert AgeCalculator.calculate_age(datetime.date(2004, 8, 25)) == 20
