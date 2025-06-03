153ebba321c6392c047d92508a1b9b4cc5e71227
Lucas Monteiro <lucasmonteiroferreira1@gmail.com>
2025-06-03 13:42:40 -0300

    Add a fixed date when testing 'calculate_age()' function

--- tests/test_age_calculator.py
+++ tests/test_age_calculator.py
@@ -2,5 +2,6 @@ from src.age_calculator import AgeCalculator
 import datetime
 
 def test_calculate_age():
-    assert AgeCalculator.calculate_age(datetime.date(1994, 10, 19)) == 30
-    assert AgeCalculator.calculate_age(datetime.date(2004, 8, 25)) == 20
+    fixed_date = datetime.date(2025, 5, 31)
+    assert AgeCalculator.calculate_age(datetime.date(1994, 10, 19), fixed_date) == 30
+    assert AgeCalculator.calculate_age(datetime.date(2004, 8, 25), fixed_date) == 20
